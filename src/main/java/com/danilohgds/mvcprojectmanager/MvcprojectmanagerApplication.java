25f780639a7febb91916c389185523a9436f9b5b
danilohgds <danilo_hgds@hotmail.com>
2024-06-08 11:42:14 -0400

    [CHORE] setup with spring.starter.io

---
+++ src/main/java/com/danilohgds/mvcprojectmanager/MvcprojectmanagerApplication.java
@@ -0,0 +1,13 @@
+package com.danilohgds.mvcprojectmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MvcprojectmanagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MvcprojectmanagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/danilohgds/mvcprojectmanager/MvcprojectmanagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.danilohgds.mvcprojectmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MvcprojectmanagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
